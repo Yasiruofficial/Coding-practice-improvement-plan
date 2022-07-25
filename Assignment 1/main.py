f516f174ba9a7fc530a5e5032ba300e278ab07a9
Yasiru Swaris <52125232+Yasiruofficial@users.noreply.github.com>
2022-07-25 15:50:09 +0530

    Update main.py

--- Assignment 1/main.py
+++ Assignment 1/main.py
@@ -1,28 +1,27 @@
-time_list = ["23:59", "00:00"]
-
+time_list = ["02:05","23:59","01:00"]
 
 def remove_semicolon(el):
-    if el == "00:00":
-        el = "24:00"
     seperated_time = el.split(":")
     time_in_min = (int(seperated_time[0]) * 60) + int(seperated_time[1])
     return time_in_min
 
-
 def find_min_minutes_def(times):
     if len(times) < 2:
-        raise ValueError("List should have at least 2 items")
-
+        raise ValueError("List should have atleast 2 items")
+    
     updated_list = list(map(remove_semicolon, times))
+    
     updated_list.sort()
+    updated_list.append(updated_list[0]+1440)
 
     minimum_minutes_def = updated_list[1] - updated_list[0]
 
     for index in range(len(updated_list) - 1):
         if updated_list[index + 1] - updated_list[index] < minimum_minutes_def:
             minimum_minutes_def = updated_list[index + 1] - updated_list[index]
-
+            
     return minimum_minutes_def
 
 
 print(find_min_minutes_def(time_list))
+# find_min_minutes_def(time_list)
