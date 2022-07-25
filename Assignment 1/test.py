e8be07b236af7b00eb1c5aab13db27f63b5b0bb1
Yasiru Swaris <52125232+Yasiruofficial@users.noreply.github.com>
2022-07-25 15:50:33 +0530

    Update test.py

--- Assignment 1/test.py
+++ Assignment 1/test.py
@@ -6,12 +6,24 @@ class TestMain(unittest.TestCase):
 
     def test_when_one_input_given(self):
         self.assertRaises(ValueError,main.find_min_minutes_def,["23:59"])
+        
+    def test_when_all_inputs_before_12(self):
+        self.assertEqual(main.find_min_minutes_def(["01:20","11:20","09:20"]), 120) #JUST
     
-    def test_when_two_input_given(self):
-        self.assertEqual(main.find_min_minutes_def(["23:59", "00:00"]), 0)
+    def test_when_all_inputs_after_12(self):
+        self.assertEqual(main.find_min_minutes_def(["13:20","23:20","21:20"]), 120) #JUST
+    
+    def test_when_time_of_other_date_is_near(self):
+        self.assertEqual(main.find_min_minutes_def(["01:20","20:20","23:55"]), 85)
+    
+    def test_when_time_of_this_date_is_near(self):
+        self.assertEqual(main.find_min_minutes_def(["01:20","23:20","23:30"]), 10)
         
-    def test_when_multiple_input_given(self):
-        self.assertEqual(main.find_min_minutes_def(["00:00","23:59","00:00"]), 0)
+    def test_when_has_duplicate_inputs_before_12(self):
+        self.assertEqual(main.find_min_minutes_def(["01:20","01:20","09:20"]), 0)
         
+    def test_when_has_duplicate_inputs_after_12(self):
+        self.assertEqual(main.find_min_minutes_def(["13:20","13:20","21:20"]), 0)
+
 
 unittest.main()
