fac9d1c4bae7dd31bd7671f614ad3b799c3008b5
Yasiru Swaris <52125232+Yasiruofficial@users.noreply.github.com>
2022-07-27 14:04:27 +0530

    Test Complete

--- Assignment 2/main.py
+++ Assignment 2/main.py
@@ -1,20 +1,16 @@
 # def maxSubArray(num_array):
-   
 #    grouping_array = [0 for i in range(len(num_array))]
 #    grouping_array[0] = num_array[0]
-   
 #    for i in range(1,len(num_array)):
 #       grouping_array[i] = max(grouping_array[i-1]+num_array[i],num_array[i])
-      
 #    return max(grouping_array)
-
-
 # num_array = [-2,1,-3,4,-1,2,1,-5,4]
-
 # print(maxSubArray(num_array))
 
 def maxSubArray(num_array):
-   max_subarray = 0
+   
+   max_subarray = num_array[0]
+   
    for i in range(len(num_array)):
       for j in range(i+1,len(num_array)+1):
          obj  = slice(i,j)
--- Assignment 2/test.py
+++ Assignment 2/test.py
@@ -4,26 +4,17 @@ import main
 
 class TestMain(unittest.TestCase):
 
-    def test_when_one_input_given(self):
-        self.assertRaises(ValueError,main.find_min_minutes_def,["23:59"])
-        
-    def test_when_all_inputs_before_12(self):
-        self.assertEqual(main.find_min_minutes_def(["01:20","11:20","09:20"]), 120) #JUST
+    def test_when_all_are_negative(self):
+        self.assertEqual(main.maxSubArray([-2,-1,-3,-4,-1,-2,-1,-5,-4]),-1)
     
-    def test_when_all_inputs_after_12(self):
-        self.assertEqual(main.find_min_minutes_def(["13:20","23:20","21:20"]), 120) #JUST
+    def test_when_all_are_positive(self):
+        self.assertEqual(main.maxSubArray([2,1,3,4,1,2,1,5,4]),23)
     
-    def test_when_time_of_other_date_is_near(self):
-        self.assertEqual(main.find_min_minutes_def(["01:20","20:20","23:55"]), 85)
-    
-    def test_when_time_of_this_date_is_near(self):
-        self.assertEqual(main.find_min_minutes_def(["01:20","23:20","23:30"]), 10)
+    def test_when_mix_array(self):
+        self.assertEqual(main.maxSubArray([-2,1,-3,4,-1,2,1,-5,4]),6)
         
-    def test_when_has_duplicate_inputs_before_12(self):
-        self.assertEqual(main.find_min_minutes_def(["01:20","01:20","09:20"]), 0)
+    def test_when_array_has_zero(self):
+        self.assertEqual(main.maxSubArray([-2,1,-3,4,0,0,1,-5,4]),5)
         
-    def test_when_has_duplicate_inputs_after_12(self):
-        self.assertEqual(main.find_min_minutes_def(["13:20","13:20","21:20"]), 0)
-
 
 unittest.main()
