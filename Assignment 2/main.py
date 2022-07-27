83ada760d4d7ef92014c87fce20116b56546e6e1
Yasiru Swaris <52125232+Yasiruofficial@users.noreply.github.com>
2022-07-27 12:55:16 +0530

    My code

--- Assignment 2/main.py
+++ Assignment 2/main.py
@@ -1,14 +1,28 @@
-def maxSubArray(num_array):
+# def maxSubArray(num_array):
    
-   grouping_array = [0 for i in range(len(num_array))]
-   grouping_array[0] = num_array[0]
+#    grouping_array = [0 for i in range(len(num_array))]
+#    grouping_array[0] = num_array[0]
    
-   for i in range(1,len(num_array)):
-      grouping_array[i] = max(grouping_array[i-1]+num_array[i],num_array[i])
+#    for i in range(1,len(num_array)):
+#       grouping_array[i] = max(grouping_array[i-1]+num_array[i],num_array[i])
       
-   return max(grouping_array)
+#    return max(grouping_array)
 
 
-num_array = [-2,1,-3,4,-1,2,1,-5,-4]
+# num_array = [-2,1,-3,4,-1,2,1,-5,4]
 
-print(maxSubArray(num_array))
+# print(maxSubArray(num_array))
+
+def maxSubArray(num_array):
+   max_subarray = 0
+   for i in range(len(num_array)):
+      for j in range(i+1,len(num_array)+1):
+         obj  = slice(i,j)
+         if sum(num_array[obj]) > max_subarray:
+            max_subarray = sum(num_array[obj])
+   return max_subarray
+            
+print(maxSubArray([-2,1,-3,4,-1,2,1,-5,4])) 
+   
+ 
+   
